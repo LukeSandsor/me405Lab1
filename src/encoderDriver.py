b4bed7c9160f8221e386e1d610b92bb12233bb3e
LukeSandsor <63250619+LukeSandsor@users.noreply.github.com>
2022-01-22 11:21:28 -0800

    Add files via upload

---
+++ src/encoderDriver.py
@@ -0,0 +1,79 @@
+'''!
+@file encoderDriver.py
+This file contains all driver functions for the encoder by scanning
+ticks of the encoder
+@author Lucas Sandsor
+@author Jack Barone
+@author Jackson Myers
+@date 10-Jan-2022 
+'''
+import pyb
+class EncoderDriver:
+    '''!
+    This class impliments and encoder for ME405
+    '''
+    def __init__(self, in1pin, in2pin, timer):
+        '''!
+        Creates an encoder driver by initializing GPIO
+        pins and reading signal output from two channels and comparing
+        them to see what the direction of movement is
+        @param in1pin    The input pin for one for one of the scanners
+        on the encoder measuring ticks
+        @param in2pin    The input pin for a different scanner seperated
+        by a half cycle from the other encoder 
+        @param timer     The timer on the STM32 that works in counter
+        mode that allows us to track ticks for the encoder
+        '''
+        print("Creating an encoder driver")
+        self.period = 4000 - 1
+        self.pinIN1 = pyb.Pin (in1pin, pyb.Pin.IN)        
+        self.pinIN2 = pyb.Pin (in2pin, pyb.Pin.IN)
+        #must declare pyb.Pin.board.en_pin in main
+        self.tim = pyb.Timer (timer, prescaler=0, freq=20000)
+        self.ch1 = self.tim.channel (1, pyb.Timer.ENC_A, pin=self.pinIN1)
+        self.ch2 = self.tim.channel (2, pyb.Timer.ENC_B, pin=self.pinIN2)
+        self.count = 0
+        self.lastCount = 0
+        self.position = 0
+        self.delta = 0
+
+    def update(self):
+        '''!
+        Handles overflow for the counter by creating a differential
+        between the last counted number and the counted number at
+        run time. If there is overflow, the differential will be
+        greater or less than the period (-self.period/2 to self.period/2
+        is equals to one period) and the code will adjust the
+        number accordingly
+        '''
+        self.count = self.tim.counter()
+        self.delta = self.count - self.lastCount
+        if self.delta > self.period/2:
+            self.delta -= self.period
+        elif self.delta < -self.period/2:
+            self.delta += self.period
+        else:
+            pass
+        self.position += self.delta
+        self.lastCount = self.count
+            
+    def read(self):
+        '''!
+        Reads the current position of the motor using the
+        past update function and then priting the value of the
+        encoder to the user
+        '''
+        self.update()
+        print("Printing Encoder Position: ", end="")
+        print(self.position)
+    
+    def zero(self):
+        '''!
+        Resets the position of the encoder to zero
+        '''
+        print("Encoder Position reset to zero!")
+        self.position = 0s
+        return 0
+        
+
+        
---
+++ src/main.py
@@ -0,0 +1,38 @@
+"""!
+@file main.py
+This file contains all the funcitons for controlling
+motor speed and couting revolutions via an encoder
+attatched to the motor
+@author Lucas Sandsor
+@author Jack Barone
+@author Jackson Myers
+@date 10-Jan-2022 
+"""
+import motorDriver
+import encoderDriver
+import pyb
+import time
+
+   
+if __name__ == "__main__":
+    '''! Initialize motor and encoder drivers and then run the motor
+    back and forth to see if the drivers and their methods were working
+    properly.
+    '''
+    moe = motorDriver.MotorDriver(pyb.Pin.board.PA10,
+        pyb.Pin.board.PB4, pyb.Pin.board.PB5, 3)
+    '''To get the motor to start to move the minumum duty cycle is 25 in either
+    direction but to keep it moving once started, the minimum duty cycle is
+    18'''
+    enc = encoderDriver.EncoderDriver(pyb.Pin.board.PB6,pyb.Pin.board.PB7, 4)
+    while(1):
+        #An infinite loop to see it go back and forth for testing
+        moe.set_duty_cycle(-25)
+        pyb.delay(1000)
+        moe.set_duty_cycle(25)
+        pyb.delay(1000)
+        moe.set_duty_cycle(0)
+        enc.read()     
+
+
+
